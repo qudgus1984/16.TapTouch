33a93f59ba83514508f8af5e8c482a4cbedf9576
ByeongHyeon <qudgus1984@naver.com>
2022-04-27 19:00:55 +0900

    16.TapTouch 완료

--- 16.TapTouch/ViewController.swift
+++ 16.TapTouch/ViewController.swift
@@ -16,7 +16,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Began"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
 
 }
 
