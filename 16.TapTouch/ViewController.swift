4315a04caae3b383f8d18ecb2b70b8a12af1a6d4
ByeongHyeon <qudgus1984@naver.com>
2022-04-27 18:47:21 +0900

    아웃렛 변수, 액션함수 추가 완료

--- 16.TapTouch/ViewController.swift
+++ 16.TapTouch/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtMessage: UILabel!
+    @IBOutlet var txtTapCount: UILabel!
+    @IBOutlet var txtTouchCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
